b74b7ec7665d3a311897522fa4a3f8d1bca1ee9e
MoonSangJin <1128msj@gmail.com>
2023-08-27 12:13:53 +0900

    💄 #5 footer 추가.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,56 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <footer className='w-full bg-stone-900 text-white p-6 flex flex-col items-center space-y-3'>
+          <div className='font-extrabold text-2xl mb-2'>HIPHOPEAT</div>
+          <div className='flex'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-7 h-7'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75'
+              />
+            </svg>
+
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-7 h-7 ml-3'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5'
+              />
+            </svg>
+
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-7 h-7 ml-3'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244'
+              />
+            </svg>
+          </div>
+          <div className='ml-auto font-semibold'>Powered by sangsang_jin</div>
+        </footer>
       </body>
     </html>
   );
