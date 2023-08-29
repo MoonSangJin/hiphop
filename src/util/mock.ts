c95cd5404b5c553274c704ba48b96027ebfce170
MoonSangJin <1128msj@gmail.com>
2023-08-29 22:42:15 +0900

    🚚 util -> utils로 폴더 이름 변경

--- src/util/mock.ts
+++
@@ -1,7 +0,0 @@
-export const MOCK_THNUMBNAIL_DATA = Object.freeze([
-  'https://img.hiphople.com/files/attach/images/1987785/831/878/025/946e225977bd77bd73488d6352c7465c.jpg',
-  'https://img.hiphople.com/files/attach/images/115990/994/820/025/f9011b2322a83170c5da7cd08e8ba3d6.jpg',
-  'https://img.hiphople.com/files/attach/images/1987785/775/932/025/5edee13df3d5021e4d4f8b75a4030786.jpg',
-  'https://img.hiphople.com/files/attach/images/115990/385/949/025/f2969e37ae1d9d330bd939367e3a5b68.jpg',
-  'https://img.hiphople.com/files/attach/images/562/456/929/025/bf8f777e5949ea7c16073626eded9dc2.jpg',
-]);
---
+++ src/utils/mock.ts
@@ -0,0 +1,95 @@
+export const MOCK_THNUMBNAIL_DATA = Object.freeze([
+  'https://img.hiphople.com/files/attach/images/1987785/831/878/025/946e225977bd77bd73488d6352c7465c.jpg',
+  'https://img.hiphople.com/files/attach/images/115990/994/820/025/f9011b2322a83170c5da7cd08e8ba3d6.jpg',
+  'https://img.hiphople.com/files/attach/images/1987785/775/932/025/5edee13df3d5021e4d4f8b75a4030786.jpg',
+  'https://img.hiphople.com/files/attach/images/115990/385/949/025/f2969e37ae1d9d330bd939367e3a5b68.jpg',
+  'https://img.hiphople.com/files/attach/images/562/456/929/025/bf8f777e5949ea7c16073626eded9dc2.jpg',
+]);
+
+export const MOCK_LATEST_THNUMBNAIL_DATA = Object.freeze([
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/294/987/025/370x220.crop.jpg?20230829043248',
+    title: '오션프롬더블루, 오피셜 믹스테입 [NEWRNBERA] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25987294',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/550/981/025/370x220.crop.jpg?20230828191640',
+    title: '미누 & 위즈 월드, 합작 EP [M1NUWORLD 2] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25981550',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/443/981/025/370x220.crop.jpg?20230828191123',
+    title: '다이나믹 듀오, 정규 10집 [2 Kids On The Block - Part.2] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25981443',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/011/980/025/370x220.crop.jpg?20230828175552',
+    title: '[The Veteran] 지코, 스윙스, 그냥노창',
+    category: '비디오',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/letv/25980011',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/498/979/025/370x220.crop.jpg?20230828173327',
+    title:
+      '피치스, 자동차 문화 기반 복합 뮤직 페스티벌 <원 유니버스 페스티벌 2023> 개최',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25979498',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/785/976/025/370x220.crop.jpg?20230828130711',
+    title: '이다옐로우, EP [Predator-Prey Role Reversal] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25976785',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/733/976/025/370x220.crop.jpg?20230828125925',
+    title: '제이비토, 믹스테입 [Buzok] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25976733',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/627/976/025/370x220.crop.jpg?20230828125042',
+    title: '루시갱, EP [C8H11NO2 CLUB] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25976627',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/331/976/025/370x220.crop.jpg?20230828133758',
+    title: '도널드 트럼프의 머그샷을 본 래퍼들의 반응',
+    category: '국외뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_world/25976331',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/264/976/025/370x220.crop.jpg?20230828115257',
+    title: '주애, EP [NO TRAGEDY!] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25976264',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/157/976/025/370x220.crop.jpg?20230828114642',
+    title: '드레이크, "다들 화난 거 압니다"',
+    category: '국외뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_world/25976157',
+  },
+  {
+    url: 'https://img.hiphople.com/files/thumbnails/119/976/025/370x220.crop.jpg?20230828112641',
+    title: '유스호스텔, 정규 2집 [Youth Hostel vol.2 Black&Orange] 발표',
+    category: '국내뉴스',
+    date: 'Aug.29',
+    link: 'https://hiphople.com/news_kr/25976119',
+  },
+]);
