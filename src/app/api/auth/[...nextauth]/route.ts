8f4d8fb9769d781be5cfcd594948ece7c688dee5
MoonSangJin <1128msj@gmail.com>
2023-09-05 23:25:22 +0900

    🐛 NextAuth Google login 안되는 문제 수정.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,5 +13,8 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
 });
 export { handler as GET, handler as POST };
