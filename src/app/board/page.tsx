33798e7d3143c4e3e1186a91be37855e272d6f7a
MoonSangJin <1128msj@gmail.com>
2023-09-12 21:53:58 +0900

    ✨ #9 로그인 되어 있을 때만 글쓰기 가능.

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import axios from 'axios';
 import BoardTable from '@/src/components/BoardTable';
-
+import { getServerSession } from 'next-auth';
 interface Post {
   id: number;
   title: string;
@@ -15,12 +15,15 @@ async function getData() {
 
 export default async function Board() {
   const posts = await getData();
+  const userInfo = await getServerSession();
   return (
     <main className='min-h-[80vh] max-w-5xl mx-auto'>
       <BoardTable />
-      <button className='bg-blue-400 float-right rounded w-20 m-2'>
-        <Link href={'board/write'}>글쓰기</Link>
-      </button>
+      {userInfo?.user && (
+        <button className='bg-blue-400 float-right rounded w-20 m-2'>
+          <Link href={'board/write'}>글쓰기</Link>
+        </button>
+      )}
     </main>
   );
 }
