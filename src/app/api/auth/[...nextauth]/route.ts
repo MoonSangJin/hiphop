d84a6c811b6d3e1c89e54b77a21bcb3414d27261
MoonSangJin <1128msj@gmail.com>
2023-09-17 19:03:47 +0900

    ✨ #9 prisma schema 수정.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -16,5 +16,25 @@ const handler = NextAuth({
   session: {
     strategy: 'jwt',
   },
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      // console.log('account');
+      // console.log(account);
+      // console.log('token');
+      // console.log(token);
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.user.accessToken = token.accessToken;
+      // console.log('ssession!!!');
+      // console.log(session);
+      return session;
+    },
+  },
 });
 export { handler as GET, handler as POST };
